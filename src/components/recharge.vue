bbbf9c1f594f890e30206c49bd4883d972e93db4
shady <shady@zy.com>
2018-04-02 14:33:20 +0800

    充值添加了两个来源

--- src/components/recharge.vue
+++ src/components/recharge.vue
@@ -25,6 +25,8 @@
               <td>序号</td>
               <td>用户名/昵称</td>
               <td>充值金额</td>
+              <td>充值类型</td>
+              <td>充值信息</td>
               <td>状态</td>
               <td>时间</td>
               <td  v-show='!one_user_recharge'>操作</td>
@@ -40,18 +42,23 @@
                   {{v.money}}
                 </td>
                 <td>
+                {{v.con}}
+                </td>
+                <td v-html="v.tp_mark" style="text-align:left;">
+                </td>
+                <td>
                   <b v-if="v.tp_stu==1" class="text-warning">待审核</b>
                   <b v-if="v.tp_stu==0" class="text-danger">充值不通过</b>
                   <b class="text-info"  v-if="v.tp_stu==2" >充值通过</b>
                 </td>
                 <td>{{v.tp_time}}</td>
-                <td v-if="v.tp_stu==1">
-                  <button  class="btn btn-primary" @click="review_recharge(v.id,2)">通过</button>
-                  <button  class="btn btn-primary" @click="review_recharge(v.id,0)">拒绝</button>
-                  <button class="btn btn-danger"  @click="delete_one(v.id)">删除</button>
+                <td v-if="v.tp_stu==1" style="text-align:right;">
+                  <button  class="btn btn-primary btn-sm" @click="review_recharge(v.id,2)">通过</button>
+                  <button  class="btn btn-primary btn-sm" @click="review_recharge(v.id,0)">拒绝</button>
+                  <button class="btn btn-danger btn-sm"  @click="delete_one(v.id)">删除</button>
                 </td>
-                 <td v-if="v.tp_stu!=1">
-                  <button class="btn btn-danger"  @click="delete_one(v.id)">删除</button>
+                 <td v-if="v.tp_stu!=1" style="text-align:right;">
+                  <button class="btn btn-danger btn-sm"  @click="delete_one(v.id)">删除</button>
                 </td>
             </tr>
           </tbody>
@@ -103,6 +110,7 @@ export default {
      */
     get_recharge_list: function() {
       this.$http.get(this.api + "/admin/topups").then(function(res) {
+        console.log(res.data);
         if (res.data.status == 200) {
           this.list = res.data.data.topups.list;
           this.list_all_back_up = res.data.data.topups.list;
